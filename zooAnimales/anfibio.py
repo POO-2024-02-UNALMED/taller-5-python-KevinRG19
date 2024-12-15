8c44b9905a4cb7f6fe60ea6a057f96e58cf38792
KevinRG19 <kerubianog@unal.edu.co>
2024-12-14 19:00:46 -0500

    intento6

---
+++ zooAnimales/anfibio.py
@@ -0,0 +1,41 @@
+from .animal import Animal
+
+class Anfibio(Animal):
+
+    _listado = []
+    ranas = 0
+    salamandras = 0
+
+    def __init__(self, nombre = '', edad = 0, habitat = '', genero = '', colorPiel = '', venenoso = False):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorPiel = colorPiel
+        self._venenoso = venenoso
+        Anfibio._listado.append(self)
+
+    def movimiento(self):
+        return "saltar"
+    
+    def getColorPiel(self):
+        return self._colorPiel
+
+    def setColorPiel(self, colorPiel):
+        self._colorPiel = colorPiel
+
+    def isVenenoso(self):
+        return self._venenoso
+
+    def setVenenoso(self, venenoso):
+        self._venenoso = venenoso
+    
+    def cantidadAnfibios(self):
+        return len(Anfibio._listado)
+
+    @classmethod
+    def crearRana(cls, nombre = '', edad = 0, genero = ''):
+        Anfibio.ranas += 1
+        return Anfibio(nombre, edad, 'selva', genero, 'rojo', True)
+    
+    @classmethod
+    def crearSalamandra(cls, nombre = '', edad = 0, genero = ''):
+        Anfibio.ranas += 1
+        return Anfibio(nombre, edad, 'selva', genero, 'negro y amarillo', False)
---
+++ zooAnimales/pez.py
@@ -0,0 +1,41 @@
+from .animal import Animal
+
+class Pez(Animal):
+
+    _listado = []
+    salmones = 0
+    bacalaos = 0
+
+    def __init__(self, nombre = '', edad = 0, habitat = '', genero = '', colorEscamas = '', cantidadAletas = 0):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorEscamas = colorEscamas
+        self._cantidadAletas = cantidadAletas
+        Pez._listado.append(self)
+
+    def movimiento(self):
+        return "nadar"
+    
+    def getColorEscamas(self):
+        return self._colorEscamas
+
+    def setColorEscamas(self, colorEscamas):
+        self._colorEscamas = colorEscamas
+
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+
+    def setCantidadAletas(self, cantidadAletas):
+        self._cantidadAletas = cantidadAletas
+    
+    def cantidadPeces(self):
+        return len(Pez._listado)
+    
+    @classmethod
+    def crearSalmon(cls, nombre = '', edad = 0, genero = ''):
+        Pez.salmones += 1
+        return Pez(nombre, edad, 'oceano', genero, 'rojo', 6)
+    
+    @classmethod
+    def crearBacalao(cls, nombre = '', edad = 0, genero = ''):
+        Pez.bacalaos += 1
+        return Pez(nombre, edad, 'oceano', genero, 'gris', 6)
